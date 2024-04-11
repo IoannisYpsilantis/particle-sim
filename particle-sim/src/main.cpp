47917c5b6f970f89a083169cb6fa15b7d37014dd
Ioannis Ypsilantis <iypsilan@stevens.edu>
2024-04-11 16:59:38 -0400

    Added option to not render simulation to screen.

--- particle-sim/src/main.cpp
+++ particle-sim/src/main.cpp
@@ -19,6 +19,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // Environment Parameters
 const int numParticles = 1000;
 const bool useCPU = true;
+const bool render = true;
+const int max_steps = -1; //Cutoff number of iterations, this is handy if rendering is false to determine a stop. Set to -1 to never terminate
 
 // Physical parameters - mass
 float proton_mass = 1.0f; //This is in atomic mass units 1amu ~ 1.67e-27 kg.
@@ -27,36 +29,16 @@ float mass_ratio = 1836.15267343f; //This is the ratio with uncertainty of 6.0e-
 
 // Physical parameters - forces
 float residual_strong_range = 3; //In fentometers (10^-15 m)
+
 // Window Parameters
 const int width = 800;
 const int height = 800;
 
 int main(int argc, char** argv) {
 
-    //Initialize GLFW 
-    glfwInit();
-
-    //This window is where we will view our graphics
-    // (width, height, title, monitor, share)
-    GLFWwindow* window = glfwCreateWindow(width, height, "Particle Simulation", NULL, NULL);
-
-    //Check to make sure window was actually created, if not exit.
-    if (!window) {
-        glfwTerminate();
-        return -1;
+    if (render == false && max_steps < 0) {
+        printf("Warning: Can only exit program via ctrl-c. This is not recommended unless testing code.\n");
     }
-
-    //Make the window the context for OpenGL
-    glfwMakeContextCurrent(window);
-
-    //Load OpenGL functions
-    gladLoadGL();
-
-    //What range of the screen we are actually drawing
-    // (0, 0, 800, 600) is the full screen given the window size of 800x600
-    glViewport(0, 0, width, height);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     float* particles_pos;
     unsigned int* particles_col;
@@ -68,59 +50,100 @@ int main(int argc, char** argv) {
         //Do GPU class initialization
     }
 
-    particles_pos = system->getPositions();
-    particles_col = system->getColors();
+    GLFWwindow* window;
+    Shader* shaderProgram;
+    Buffer* buffers;
 
-    Shader shaderProgram;
+    if (render) {
+        //Initialize GLFW 
+        glfwInit();
 
-    Buffer buffers(particles_pos, particles_col, 1000);
+        //This window is where we will view our graphics
+        // (width, height, title, monitor, share)
+        window = glfwCreateWindow(width, height, "Particle Simulation", NULL, NULL);
 
-    //Specify the color of the window
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        //Check to make sure window was actually created, if not exit.
+        if (!window) {
+            glfwTerminate();
+            delete system;
+            return -1;
+        }
 
-    //Clear the window to the color we set in glClearColor
-    glClear(GL_COLOR_BUFFER_BIT);
+        //Make the window the context for OpenGL
+        glfwMakeContextCurrent(window);
 
-    //Swap the buffers so we see the new frame generated.
-    glfwSwapBuffers(window);
-    GLint m_viewport[4];
+        //Load OpenGL functions
+        gladLoadGL();
+
+        //What range of the screen we are actually drawing
+        // (0, 0, 800, 600) is the full screen given the window size of 800x600
+        glViewport(0, 0, width, height);
+
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+        particles_pos = system->getPositions();
+        particles_col = system->getColors();
+
+        shaderProgram = new Shader();
+
+        buffers = new Buffer(particles_pos, particles_col, 1000);
+    }
+    
 
     
+
+    int steps = 0;
+    
     //This loop runs until the window is closed (or I guess if we make the program exit somehow)
-    while(!glfwWindowShouldClose(window)) {
-        //glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        //Use the shader program
-        shaderProgram.Activate();
-
-        glDrawArrays(GL_POINTS, 0, 1000);
-
-        //Swap the buffers so we see the new frame generated.
-        glfwSwapBuffers(window);
-
-        //We want to draw the points onto the screen:
-        glfwPollEvents();
-        
+    while (steps != max_steps && (!render || !glfwWindowShouldClose(window))) {
         system->update(0);
+        
 
-        buffers.updatePositions(particles_pos, 1000);
+        if (render) {
+            //glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            //Use the shader program
+            shaderProgram->Activate();
+
+            glDrawArrays(GL_POINTS, 0, 1000);
+
+            //Swap the buffers so we see the new frame generated.
+            glfwSwapBuffers(window);
+
+            //We want to draw the points onto the screen:
+            glfwPollEvents();
+
+            buffers->updatePositions(particles_pos, 1000);
+        }
+
+        steps++;
+        
     }
 
     //We have completed so we need to clean up.
-    //Delete the VBO and VAO
-    buffers.Delete();
+    
+    if (render) {
+        //Delete the VBO and VAO
+        buffers->Delete();
+        delete buffers;
 
-    //Delete the shader program
-    shaderProgram.Delete();
+        //Delete the shader program
+        shaderProgram->Delete();
+        delete shaderProgram;
+
+        //Terminate GLFW
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    
 
     //Delete the particleSystem
     delete system;
-
-    //Terminate GLFW
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    
     return 0;
 
 }
